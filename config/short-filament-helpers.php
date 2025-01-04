6c34015a79cc0ff7b02d9811367978a0a008cf8b
MHortulanus <manoj@vormkracht10.nl>
2025-01-04 16:15:50 +0100

    wip

--- config/short-filament-helpers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for ManojHortulanus/ShortFilamentHelpers
-return [
-
-];
--- src/ShortFilamentHelpersServiceProvider.php
+++ src/ShortFilamentHelpersServiceProvider.php
@@ -16,10 +16,6 @@ class ShortFilamentHelpersServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('short-filament-helpers')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_short_filament_helpers_table')
-            ->hasCommand(ShortFilamentHelpersCommand::class);
+            ->name('short-filament-helpers');
     }
 }
